b009fab990d704c0b19229c214295ef51e7edce1
이준규 <jkjkjk5710@gmail.com>
2022-05-11 17:56:31 +0900

    gateway

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+- http vs socket
+  - http: 단방향, 폴링방식
+  - socket: emit - on 소통
+
+---
+
 <p align="center">
   <a href="http://nestjs.com/" target="blank"><img src="https://nestjs.com/img/logo-small.svg" width="200" alt="Nest Logo" /></a>
 </p>
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,27 @@
-import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
+import { Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
-// import { AppService } from './app.service';
+import * as mongoose from 'mongoose';
 
 @Module({
-  imports: [ConfigModule.forRoot(
-    {
-      isGlobal: true
-    }
-  )],
-  controllers: [AppController],
-  providers: [
-    // AppService
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }),
   ],
+  controllers: [AppController],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure() {
+    const DEBUG = process.env.MODE === 'dev' ? true : false;
+    mongoose.set('debug', DEBUG);
+  }
+}
